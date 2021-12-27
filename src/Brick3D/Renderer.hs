a5b11e613920d538c31fc43651fe1b36ad540905
Cj-bc <cj.bc-sd@outlook.jp>
2021-12-27 15:25:35 +0900

    Change: Split render function into pure and non-pure functions
    It's much easier to maintain and write tests

--- src/Brick3D/Renderer.hs
+++ src/Brick3D/Renderer.hs
@@ -4,11 +4,18 @@ import Brick3D.Camera
 -- | Renders 'ThreeDState' to one 'Tart.Canvas.Canvas',
 -- which will be shown in 'Widget'
 render  :: MonadIO m => ThreeDState -> m ThreeDState
-render = do
-  -- Convert to viewport coordinate
-  let focalLength = 
-  -- Convert to device coordinate
+render s = do
+  let rasteriezd = render' s
+  screen' <- liftIO . canvasSetMany rasterized $ s^.screen
+  pure $ s&screen.~screen'
 
+-- | Do rendering process other than IO operation (which is done in 'render'
+render' :: ThreeDState -> [((Int, Int), Char, Attr)]
+render' s =
+  -- Convert to viewport coordinate
+  let cam = s^.cam
+      focalLength = 1/(tan $ (cam^.hFov)/2)
+      -- Convert to device coordinate
   -- Geometry Construction
     -- Also, calculate Normal for later use(e.g. shading)
   -- Shading by using property
