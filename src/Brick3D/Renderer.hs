b572ce9ad5bce66700d2dad310a70ff941d7c441
Cj-bc <cj.bc-sd@outlook.jp>
2021-12-29 01:17:29 +0900

    Add: Brick3D.Renderer> implement Primitive projection

--- src/Brick3D/Renderer.hs
+++ src/Brick3D/Renderer.hs
@@ -3,6 +3,9 @@ import Brick3D.State
 import Brick3D.Camera
 import Brick3D.Type
 
+import Data.Vector (Vector)
+import Lens.Micro.Platform
+import Linear.V3 (_z)
 -- | Renders 'ThreeDState' to one 'Tart.Canvas.Canvas',
 -- which will be shown in 'Widget'
 render  :: MonadIO m => ThreeDState -> m ThreeDState
@@ -18,9 +21,23 @@ render' s =
   let cam = s^.camera
       focalLength = 1/(tan $ (cam^.hFov)/2)
       -- Convert to device coordinate
+      -- Also, calculate Normal for later use(e.g. shading)
+      dcprims :: Vector DCPrimitive
+      dcprims = projectPrimitive focalLength <$> s^.prims
   -- Geometry Construction
-    -- Also, calculate Normal for later use(e.g. shading)
   -- Shading by using property
   -- Rasterize
 
-convertToDeviceCoordinate :: Float -> Vertex -> V2 Float
+
+-- | Project one 'Primitive' to device coordinate
+projectPrimitive :: Float -> Primitive -> DCPrimitive
+projectPrimitive focalLength prim =
+  let prim' = over (vertices) (projectVertex focalLength) prim
+      norm = calcNormal prim
+  in DCPrimitive prim' norm
+
+-- | Project one vertex to device coordinate
+projectVertex :: Float -> Vertex -> Vertex
+projectVertex focalLength v = 
+  let percentage = focalLength/(v^.(v_normal._z))
+  in v&v_normal%~(fmap (* percentage))
