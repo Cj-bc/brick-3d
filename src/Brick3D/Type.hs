ed7d33c284bc91206c64da95da799b7a08bc2bd8
Cj-bc <cj.bc-sd@outlook.jp>
2022-01-03 22:50:29 +0900

    Add: Brick3D.Type> Create other type for Device coordinate objects

--- src/Brick3D/Type.hs
+++ src/Brick3D/Type.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Brick3D.Type where
-import Linear.V3 (V3(..), cross)
+import Linear.V3 (V3(..), cross, _x, _y, _z)
+import Linear.V2 (V2(..))
 import Linear.Matrix (M33(..))
 import Lens.Micro.Platform
 
 type Position = V3 Float
 type Rotation = M33 Float
+-- | Position in Device Coordinate
+type DCPosition = V2 Float
 type Normal   = V3 Float
 
 -- | One 'Vertex'
@@ -13,6 +16,18 @@ data Vertex = Vertex { _v_position :: Position
                      -- We might add more information as Vertex attribute
                      } deriving (Show, Eq, Ord)
 makeLenses ''Vertex
+
+-- | 'Vertex' that is mapped to Device Coordinate
+data DCVertex = DCVertex { _dcv_position :: DCPosition
+                         -- | Stores depth Z value of this primitive
+                         , _zBuffer :: Float
+                         } deriving (Show, Eq, Ord)
+makeLenses ''DCVertex
+
+-- | Convert 'Vertex' to 'DCVertex'
+fromVertex :: Vertex -> DCVertex
+fromVertex v = DCVertex (V2 (v^.v_position._x) (v^.v_position._y)) (v^.v_position._z)
+
 
 data Primitive = Point Vertex
                -- | Line (Vector Vertex)
