394ac072317d0688ab463d6512f776a59aafe0c8
Cj-bc <cj.bc-sd@outlook.jp>
2022-01-12 17:09:23 +0900

    [WIP] Add: Brick3D.Rasterization> fillTriangle to generate DCVerteces that fills triangle
    This is still buggy, doesn't work properly.

--- src/Brick3D/Rasterization.hs
+++ src/Brick3D/Rasterization.hs
@@ -3,12 +3,15 @@ import Brick3D.Type
 
 import Data.Map (Map)
 import Data.Bool (bool)
+import Data.Vector (Vector)
+import qualified Data.Vector as V
 import qualified Data.Map as M
 import Graphics.Vty.Attributes (Attr, defAttr)
 import Lens.Micro.Platform
 import Linear.V2 (V2(..))
 import Linear.V3 (_x, _y, _z)
 import Linear.Vector ((^*), (^+^))
+import Linear.Metric (dot, distance)
 
   -- | Represents one Pixel
 type PixelAttr = (Char, Attr)
@@ -55,3 +58,46 @@ rasterizeLine begin end = let v = end^.dcv_position - begin^.dcv_position :: V2 
                               formula t = (begin^.dcv_position) ^+^ (v ^* t)
                               ts = fmap (/ 500) [0..500] :: [Float]
                           in fmap (\t -> begin&dcv_position.~(formula t)) ts
+
+-- | Returns 'DCVertex's that constructs one filled-triangle
+--
+-- Note that depth of those 'DCVertex's are not calculated properly.
+-- Currently it inherits depth of 'v1'
+-- I want to fix this later.
+fillTriangle :: DCVertex -> DCVertex -> DCVertex -> Vector DCVertex
+fillTriangle v1 v2 v3 = flip (dcv_position.~) v1
+                        <$> V.filter (`isInsideOfTri` (v1^.dcv_position, v2^.dcv_position, v3^.dcv_position))
+                        boundaryRectVertices
+  where
+    maxX = foldr1 max $ (^.dcv_position._x) <$> [v1, v2, v3]
+    minX = foldr1 min $ (^.dcv_position._x) <$> [v1, v2, v3]
+    maxY = foldr1 max $ (^.dcv_position._y) <$> [v1, v2, v3]
+    minY = foldr1 min $ (^.dcv_position._y) <$> [v1, v2, v3]
+    boundaryRectVertices = V.fromList [V2 (x/100) (y/100) | x <- [(minX*100)..(maxX*100)]
+                                                          , y <- [(minY*100)..(maxY*100)]]
+
+-- | 'True' if given coordinate is within given Triangle
+--
+-- It doesn't care if the triangle is formed well.
+-- (e.g. one vertex is on the line made of other two vertex, two of them are the same.)
+-- This is because projection could
+--
+-- JP: この関数は「三角形がきちんと三角形であるか」を考慮しません。
+-- (例えば: 頂点が一直線上に並んでしまっている, 複数の頂点が同じ位置にあるなど)
+isInsideOfTri :: V2 Float -> (V2 Float, V2 Float, V2 Float) -> Bool
+isInsideOfTri candidate (v1, v2, v3)
+  = all (oneLineTest candidate)  [(v1, v2, v3), (v2, v3, v1), (v3, v1, v2)]
+  where
+    -- | Test wether 'cand' and 'a' is at the same side of line made of vs and ve
+    --
+    -- JP: 'cand' と 'a' が, vs,ve を両端に持つ直線の同じ側にあるかを判定する。
+    -- 同じ側にあれば, その直線と 'cand' 及び 'a' の内積の符号が等しくなる=0以上になる
+    -- はずなので判定ができる。
+    oneLineTest :: V2 Float -> (V2 Float, V2 Float, V2 Float) -> Bool
+    oneLineTest cand (vs, ve, a) = (vl `dot` cand)*(vl `dot` a) >= 0 -- 線上にある場合も含めている
+      where
+        -- | JP: 確かめたい対象の直線ベクトル
+        line = ve - vs
+        -- | JP: 対象の線分に直交する直線ベクトル
+        -- 直交する場合内積が0になる性質を利用して定義する
+        vl = V2 (line^._y) (-line^._x)
