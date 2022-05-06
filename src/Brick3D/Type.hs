6b9c0aeaeb87178f0e17476fb0654d9fb07909c3
Cj-bc <cj.bc-sd@outlook.jp>
2022-05-06 22:30:26 +0900

    Docs: Add: Brick3D.Type> Wrote some documentation comment

--- src/Brick3D/Type.hs
+++ src/Brick3D/Type.hs
@@ -39,12 +39,28 @@ type Position = V3 Float
 -- | Rotation in 3-Dimension
 type Rotation = M33 Float
 -- | Position in Device Coordinate
+--
+-- Device coordnate is a Coordinate that
+-- put (0,0) on the center, and Y axis
+-- is positive above the center. X axis
+-- is positive on the right.
+-- The value have range of -1~1
+-- and floating number(As shown in the Type
+-- definition)
+
+-- This is the same as what OpenGL explains
+-- <<https://open.gl/drawing#Vertex input>>
+-- 
+-- デバイス座標系は,画面の中心を原点とする
+-- 座標系で, 原点から上に向かってY軸が伸び,
+-- 右に向かってX軸が伸びています。
+-- 値は-1~1の間の小数を取ります。
 type DCPosition = V2 Float
 -- | Normal of some 3-Dimensional object
 type Normal   = V3 Float
 
 
--- | One 'Vertex'
+-- | One 'Vertex' in 3D coordinate
 data Vertex = Vertex { _v_position :: Position
                      -- We might add more information as Vertex attribute
                      } deriving (Show, Eq, Ord)
